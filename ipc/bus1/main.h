teg/bus1

commit 6c4da221fab98441d757b5f59a82e9be90e3831d

user: don't lock around charges/discharges
Each of the operations is atomic, though the charge as a whole no
longer is.

The effect of this is that it would now be possible for two
simultaneous charges to both fail, whereas (at most) one would have
succeeded before this change. This is an edge-case that we don't
particularly care to support.

Signed-off-by: Tom Gundersen <teg@jklm.no>

ipc/bus1/user.c
@@ -390,14 +390,14 @@ static int bus1_user_charge_one(atomic_t *global_remaining,
 	return 0;
 }
 
-static int bus1_user_charge_quota_locked(struct bus1_user_usage *q_global,
-					 struct bus1_user_usage *q_local,
-					 struct bus1_user_limits *l_global,
-					 struct bus1_user_limits *l_local,
-					 int n_slices,
-					 int n_handles,
-					 int n_bytes,
-					 int n_fds)
+static int bus1_user_charge_quota_apply(struct bus1_user_usage *q_global,
+					struct bus1_user_usage *q_local,
+					struct bus1_user_limits *l_global,
+					struct bus1_user_limits *l_local,
+					int n_slices,
+					int n_handles,
+					int n_bytes,
+					int n_fds)
 {
 	int r;
 
@@ -473,6 +473,10 @@ revert_slices:
  * all fail. Hence, it makes little sense to mix negative and positive charges
  * in a single call.
  *
+ * Note that this is not atomic, it may happen that trying to apply two charges
+ * simultaneously might fail whereas applying them one at a time would allow one
+ * to succeed.
+ *
  * Return: 0 on success, negative error code on failure.
  */
 int bus1_user_charge_quota(struct bus1_user *user,
@@ -489,7 +493,6 @@ int bus1_user_charge_quota(struct bus1_user *user,
 	WARN_ON(n_slices < 0 || n_handles < 0 || n_bytes < 0 || n_fds < 0);
 
 	mutex_lock(&user->lock);
-
 	usage = bus1_user_limits_map(limits, actor);
 	if (IS_ERR(usage)) {
 		r = PTR_ERR(usage);
@@ -501,11 +504,14 @@ int bus1_user_charge_quota(struct bus1_user *user,
 		r = PTR_ERR(u_usage);
 		goto exit;
 	}
+	mutex_unlock(&user->lock);
 
-	r = bus1_user_charge_quota_locked(u_usage, usage, &user->limits,
-					  limits, n_slices, n_handles,
-					  n_bytes, n_fds);
+	r = bus1_user_charge_quota_apply(u_usage, usage, &user->limits, limits,
+					 n_slices, n_handles, n_bytes, n_fds);
+	if (r < 0)
+		return r;
 
+	return 0;
 exit:
 	mutex_unlock(&user->lock);
 	return r;
@@ -538,14 +544,12 @@ void bus1_user_discharge_quota(struct bus1_user *user,
 	WARN_ON(n_slices < 0 || n_handles < 0 || n_bytes < 0 || n_fds < 0);
 
 	mutex_lock(&user->lock);
-
 	q_local = bus1_user_limits_map(l_local, actor);
-	if (WARN_ON(IS_ERR(q_local)))
-		goto exit;
-
 	q_global = bus1_user_limits_map(&user->limits, actor);
-	if (WARN_ON(IS_ERR(q_global)))
-		goto exit;
+	mutex_unlock(&user->lock);
+
+	WARN_ON(IS_ERR(q_local));
+	WARN_ON(IS_ERR(q_global));
 
 	atomic_sub(n_slices, &q_global->n_slices);
 	atomic_sub(n_handles, &q_global->n_handles);
@@ -566,9 +570,6 @@ void bus1_user_discharge_quota(struct bus1_user *user,
 	atomic_add(n_handles, &l_local->n_handles);
 	atomic_add(n_bytes, &l_local->n_inflight_bytes);
 	atomic_add(n_fds, &l_local->n_inflight_fds);
-
-exit:
-	mutex_unlock(&user->lock);
 }
 
 /**
@@ -598,14 +599,12 @@ void bus1_user_commit_quota(struct bus1_user *user,
 	WARN_ON(n_slices < 0 || n_handles < 0 || n_bytes < 0 || n_fds < 0);
 
 	mutex_lock(&user->lock);
-
 	q_local = bus1_user_limits_map(l_local, actor);
-	if (WARN_ON(IS_ERR(q_local)))
-		goto exit;
-
 	q_global = bus1_user_limits_map(&user->limits, actor);
-	if (WARN_ON(IS_ERR(q_global)))
-		goto exit;
+	mutex_unlock(&user->lock);
+
+	WARN_ON(IS_ERR(q_local));
+	WARN_ON(IS_ERR(q_global));
 
 	atomic_sub(n_slices, &q_global->n_slices);
 	atomic_sub(n_handles, &q_global->n_handles);
@@ -622,7 +621,4 @@ void bus1_user_commit_quota(struct bus1_user *user,
 
 	atomic_add(n_bytes, &l_local->n_inflight_bytes);
 	atomic_add(n_fds, &l_local->n_inflight_fds);
-
-exit:
-	mutex_unlock(&user->lock);
 }

commit 6049c5ae83c69ec3b95b0f38781c99d42e629fa9

user: fix a race in access to the usage objects
When charging a quota, we first fetch the current usage, then
determine if the quota can be charged, and then update the usage.

This used to be protected by the user lock, but that broke in
6c4da221fab98441d757b5f59a82e9be90e3831d. Restore the correct
behaviour by adding a per-usage lock.

Signed-off-by: Tom Gundersen <teg@jklm.no>

ipc/bus1/main.h
@@ -59,6 +59,7 @@
  *   bus1_peer.active
  *     bus1_peer.local.lock
  *       bus1_peer.data.lock
+ *       bus1_user_usage.lock
  *       bus1_user_limits.lock
  *       bus1_user_lock
  */

ipc/bus1/user.c
@@ -70,6 +70,8 @@ static struct bus1_user_usage *bus1_user_usage_new(void)
 	if (!usage)
 		return ERR_PTR(-ENOMEM);
 
+	mutex_init(&usage->lock);
+
 	return usage;
 }
 
@@ -81,6 +83,7 @@ bus1_user_usage_free(struct bus1_user_usage *usage)
 		WARN_ON(atomic_read(&usage->n_handles));
 		WARN_ON(atomic_read(&usage->n_bytes));
 		WARN_ON(atomic_read(&usage->n_fds));
+		mutex_destroy(&usage->lock);
 		kfree(usage);
 	}
 
@@ -424,6 +427,7 @@ int bus1_user_charge_quota(struct bus1_user *user,
 	if (IS_ERR(usage))
 		return PTR_ERR(usage);
 
+	mutex_lock(&usage->lock);
 	r = bus1_user_charge_quota_one(&limits->n_slices,
 				 atomic_read(&usage->n_slices),
 				 n_slices);
@@ -453,6 +457,8 @@ int bus1_user_charge_quota(struct bus1_user *user,
 	atomic_add(n_bytes, &usage->n_bytes);
 	atomic_add(n_fds, &usage->n_fds);
 
+	mutex_unlock(&usage->lock);
+
 	return 0;
 
 revert_bytes:
@@ -461,6 +467,7 @@ revert_handles:
 	atomic_add(n_handles, &limits->n_handles);
 revert_slices:
 	atomic_add(n_slices, &limits->n_slices);
+	mutex_unlock(&usage->lock);
 	return r;
 }
 

ipc/bus1/user.h
@@ -49,12 +49,14 @@
  * @n_handles:			number of used handles
  * @n_bytes:			number of used bytes
  * @n_fds:			number of used fds
+ * @lock:			object lock
  */
 struct bus1_user_usage {
 	atomic_t n_slices;
 	atomic_t n_handles;
 	atomic_t n_bytes;
 	atomic_t n_fds;
+	struct mutex lock;
 };
 
 /**